11385e4d1282cfef5442f97c917df5c903beebf7
Sergio Mironescu <sergiu.mironescu@gmail.com>
2014-11-03 16:13:46 +0100

    Added fizzbuzz v2

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -1,11 +1,15 @@
 count = 0
 while count < 100
-  if count % 3 == 0
-    puts "Fizz"
-  elsif count % 5 == 0
-    puts "Buzz"
-  elsif count % 3 == 0 && count % 5 == 0
+  if (count % 5) == 0 && (count % 3) == 0
     puts "FizzBuzz"
+  elsif (count % 3) == 0
+    puts "Fizz"
+  elsif (count % 5) == 0
+    puts "Buzz".reverse
+  elsif count.even?
+    puts "numero par"
+  elsif count.odd?
+    puts "numero impar"
   else
     puts count
   end
